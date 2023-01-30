771f2bb45ecdb86d78dd16f6091ddb0077d40f59
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-01-30 11:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predict():
         return render_template('index.html', prediction="Something went wrong")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=$PORT,debug=True)
